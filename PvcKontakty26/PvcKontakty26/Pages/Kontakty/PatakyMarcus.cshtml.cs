75a12e51eb68c51488d6e5f0727878cb6fbbd093
ou4pc12 <pataky.marcus@sspvc.cz>
2026-01-19 08:51:23 +0100

    Přidána kontaktní stránka: Pataky Marcus

---
+++ PvcKontakty26/PvcKontakty26/Pages/Kontakty/PatakyMarcus.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PvcKontakty26.Pages.Kontakty
+{
+    public class PatakyMarcusModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
